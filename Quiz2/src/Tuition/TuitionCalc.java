74b7646fa9de477a189b5a40ef2247c45dc3f187
Kevin <Kevin@10.0.0.201>
2016-09-21 00:37:11 -0400

    Second revision

--- Quiz2/src/Tuition/TuitionCalc.java
+++ Quiz2/src/Tuition/TuitionCalc.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TuitionCalc {
 
 	public static void main(String[] args) {
-		double initial_tuition, per_change, tuition_cost;
+		double initial_tuition, per_change, tuition_cost = 0;
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("What is the tuition cost intially: ");
@@ -17,11 +17,11 @@ public class TuitionCalc {
 		in.close();
 		
 		for (int i = 0; i <= 3; i++) {
-			tuition_cost = (initial_tuition * Math.pow(1 + per_change, i));
+			tuition_cost += (initial_tuition * Math.pow(1 + per_change, i));}
 
-		System.out.printf("Total Cost of Degree: $%.2f" + tuition_cost);
+		System.out.printf("Total Cost of Degree: $%.2f", tuition_cost);
 
 		}
 	}
 
-}
+
