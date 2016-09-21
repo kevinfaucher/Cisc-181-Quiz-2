fe790cb53b3ec8f6614c16de607af59346e3e9ab
Kevin <Kevin@10.0.0.201>
2016-09-20 21:40:08 -0400

    Quiz 2

---
+++ Quiz2/src/Tuition/TuitionCalc.java
@@ -0,0 +1,27 @@
+package Tuition;
+
+import java.util.Scanner;
+
+public class TuitionCalc {
+
+	public static void main(String[] args) {
+		double initial_tuition, per_change, tuition_cost;
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("What is the tuition cost intially: ");
+		initial_tuition = in.nextDouble();
+		
+		System.out.println("What is the percent increase in tuition annually: ");
+		per_change = in.nextDouble();
+		
+		in.close();
+		
+		for (int i = 0; i <= 3; i++) {
+			tuition_cost = (initial_tuition * Math.pow(1 + per_change, i));
+
+		System.out.printf("Total Cost of Degree: $%.2f" + tuition_cost);
+
+		}
+	}
+
+}
